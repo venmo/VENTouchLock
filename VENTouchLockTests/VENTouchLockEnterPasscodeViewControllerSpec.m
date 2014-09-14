ce9e7dca730e07468ce807743e7a3653c5d5c338
Dasmer Singh <dasmer.singh@venmo.com>
2014-09-14 02:24:54 -0400

    Test recordIncorrectPasscodeAttempt

--- VENTouchLockTests/VENTouchLockEnterPasscodeViewControllerSpec.m
+++ VENTouchLockTests/VENTouchLockEnterPasscodeViewControllerSpec.m
@@ -1,8 +1,25 @@
 #import "VENTouchLockEnterPasscodeViewController.h"
+#import "VENTouchLock.h"
 
-@interface VENTouchLockEnterPasscodeViewController (Internal)
+static id mockTouchLock = nil;
+
+@implementation VENTouchLock (UnitTests)
+
+#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
++ (instancetype)sharedInstance
+{
+    return mockTouchLock;
+}
+
+@end
+
+@interface VENTouchLockEnterPasscodeViewController (UnitTests)
 
 extern NSString *const VENTouchLockEnterPasscodeUserDefaultsKeyNumberOfConsecutivePasscodeAttempts;
+
+- (void)recordIncorrectPasscodeAttempt;
+
+- (void)callExceededLimitActionBlock;
 
 @end
 
@@ -23,6 +40,56 @@ describe(@"resetPasscodeAttemptHistory:", ^{
         [VENTouchLockEnterPasscodeViewController resetPasscodeAttemptHistory];
         NSUInteger afterAttemptHistory = [[NSUserDefaults standardUserDefaults] integerForKey:VENTouchLockEnterPasscodeUserDefaultsKeyNumberOfConsecutivePasscodeAttempts];
         expect(afterAttemptHistory).to.equal(0);
+    });
+
+});
+
+describe(@"recordIncorrectPasscodeAttempt", ^{
+
+    __block id mockEnterPasscodeVC;
+
+    beforeEach(^{
+        mockTouchLock = [OCMockObject mockForClass:[VENTouchLock class]];
+        [[[mockTouchLock stub] andReturnValue:OCMOCK_VALUE((NSUInteger){3})] passcodeAttemptLimit];
+        VENTouchLockEnterPasscodeViewController *enterPasscodeVC = [[VENTouchLockEnterPasscodeViewController alloc] init];
+        mockEnterPasscodeVC = [OCMockObject partialMockForObject:enterPasscodeVC];
+    });
+
+    afterEach(^{
+        mockTouchLock = nil;
+    });
+
+    it(@"should not call callExceededLimitActionBlock when incorrectPasscodeAttempt < passcodeAttemptLimit", ^{
+               [[mockEnterPasscodeVC reject] callExceededLimitActionBlock];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+        [mockEnterPasscodeVC verify];
+    });
+
+    it(@"should call callExceededLimitActionBlock when incorrectPasscodeAttempt = passcodeAttemptLimit", ^{
+        VENTouchLockEnterPasscodeViewController *enterPasscodeVC = [[VENTouchLockEnterPasscodeViewController alloc] init];
+        id mockEnterPasscodeVC = [OCMockObject partialMockForObject:enterPasscodeVC];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+
+        [[mockEnterPasscodeVC expect] callExceededLimitActionBlock];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+        [mockEnterPasscodeVC verify];
+    });
+
+    it(@"should call callExceededLimitActionBlock when incorrectPasscodeAttempt > passcodeAttemptLimit", ^{
+        VENTouchLockEnterPasscodeViewController *enterPasscodeVC = [[VENTouchLockEnterPasscodeViewController alloc] init];
+        id mockEnterPasscodeVC = [OCMockObject partialMockForObject:enterPasscodeVC];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+
+        [[mockEnterPasscodeVC expect] callExceededLimitActionBlock];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+        [mockEnterPasscodeVC verify];
+
+        [[mockEnterPasscodeVC expect] callExceededLimitActionBlock];
+        [mockEnterPasscodeVC recordIncorrectPasscodeAttempt];
+        [mockEnterPasscodeVC verify];
     });
 
 });
