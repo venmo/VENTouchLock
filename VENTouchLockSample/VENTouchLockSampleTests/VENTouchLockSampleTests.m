f21d7d5c7913d720e7942e0244e8db3ab8d2abb0
Dasmer Singh <dasmer.singh@venmo.com>
2014-09-29 18:41:49 -0400

    Create integration tests

--- VENTouchLockSample/VENTouchLockSampleTests/VENTouchLockSampleTests.m
+++ VENTouchLockSample/VENTouchLockSampleTests/VENTouchLockSampleTests.m
@@ -1,13 +1,6 @@
-//
-//  VENTouchLockSampleTests.m
-//  VENTouchLockSampleTests
-//
-//  Created by Dasmer Singh on 8/18/14.
-//  Copyright (c) 2014 Venmo. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <KIF/KIF.h>
+#import "VENTouchLock.h"
 
 @interface VENTouchLockSampleTests : XCTestCase
 
@@ -15,19 +8,98 @@
 
 @implementation VENTouchLockSampleTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testBasicCreatePasswordFlow
+{
+    [self dismissAutoLockAndDeletePasscode];
+    VENTouchLockAppearance *currentAppearance = [VENTouchLock sharedInstance].appearance;
+    [tester tapViewWithAccessibilityLabel:@"Set Passcode"];
+    [tester waitForKeyboard];
+    [tester enterTextIntoCurrentFirstResponder:@"1"];
+    [tester enterTextIntoCurrentFirstResponder:@"2"];
+    [tester enterTextIntoCurrentFirstResponder:@"3"];
+    [tester enterTextIntoCurrentFirstResponder:@"4"];
+    [tester waitForViewWithAccessibilityLabel:currentAppearance.createPasscodeConfirmLabelText];
+    [tester enterTextIntoCurrentFirstResponder:@"1"];
+    [tester enterTextIntoCurrentFirstResponder:@"2"];
+    [tester enterTextIntoCurrentFirstResponder:@"3"];
+    [tester enterTextIntoCurrentFirstResponder:@"4"];
+    [tester waitForAbsenceOfKeyboard];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)testBasicEnterPasscodeFlow
+{
+    [self dismissAutoLockAndDeletePasscode];
+    [[VENTouchLock sharedInstance] setPasscode:@"7890"];
+    [tester tapViewWithAccessibilityLabel:@"Show Passcode"];
+    [tester waitForKeyboard];
+    [tester enterTextIntoCurrentFirstResponder:@"7"];
+    [tester enterTextIntoCurrentFirstResponder:@"8"];
+    [tester enterTextIntoCurrentFirstResponder:@"9"];
+    [tester enterTextIntoCurrentFirstResponder:@"0"];
+    [tester waitForAbsenceOfKeyboard];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testAdvancedCreatePasscodeFlow
+{
+    [self dismissAutoLockAndDeletePasscode];
+    VENTouchLockAppearance *currentAppearance = [VENTouchLock sharedInstance].appearance;
+    [tester tapViewWithAccessibilityLabel:@"Set Passcode"];
+    [tester waitForKeyboard];
+    [tester enterTextIntoCurrentFirstResponder:@"1"];
+    [tester enterTextIntoCurrentFirstResponder:@"2"];
+    [tester enterTextIntoCurrentFirstResponder:@"3"];
+    [tester enterTextIntoCurrentFirstResponder:@"4"];
+    [tester waitForViewWithAccessibilityLabel:currentAppearance.createPasscodeConfirmLabelText];
+    [tester enterTextIntoCurrentFirstResponder:@"1"];
+    [tester enterTextIntoCurrentFirstResponder:@"2"];
+    [tester enterTextIntoCurrentFirstResponder:@"3"];
+    [tester enterTextIntoCurrentFirstResponder:@"1"];
+    [tester waitForViewWithAccessibilityLabel:currentAppearance.createPasscodeMismatchedLabelText];
+    [tester enterTextIntoCurrentFirstResponder:@"1"];
+    [tester enterTextIntoCurrentFirstResponder:@"2"];
+    [tester enterTextIntoCurrentFirstResponder:@"3"];
+    [tester enterTextIntoCurrentFirstResponder:@"4"];
+    [tester waitForViewWithAccessibilityLabel:currentAppearance.createPasscodeConfirmLabelText];
+    [tester enterTextIntoCurrentFirstResponder:@"1"];
+    [tester enterTextIntoCurrentFirstResponder:@"2"];
+    [tester enterTextIntoCurrentFirstResponder:@"3"];
+    [tester enterTextIntoCurrentFirstResponder:@"4"];
+    [tester waitForAbsenceOfKeyboard];
+}
+
+- (void)testAdvancedEnterPasscodeFlow
+{
+    [self dismissAutoLockAndDeletePasscode];
+    VENTouchLockAppearance *currentAppearance = [VENTouchLock sharedInstance].appearance;
+    [[VENTouchLock sharedInstance] setPasscode:@"7890"];
+    [tester tapViewWithAccessibilityLabel:@"Show Passcode"];
+    [tester waitForKeyboard];
+    [tester enterTextIntoCurrentFirstResponder:@"7"];
+    [tester enterTextIntoCurrentFirstResponder:@"8"];
+    [tester enterTextIntoCurrentFirstResponder:@"9"];
+    [tester enterTextIntoCurrentFirstResponder:@"9"];
+    [tester waitForViewWithAccessibilityLabel:currentAppearance.enterPasscodeIncorrectLabelText];
+    [tester enterTextIntoCurrentFirstResponder:@"7"];
+    [tester enterTextIntoCurrentFirstResponder:@"8"];
+    [tester enterTextIntoCurrentFirstResponder:@"9"];
+    [tester enterTextIntoCurrentFirstResponder:@"0"];
+    [tester waitForAbsenceOfKeyboard];
+}
+
+
+#pragma mark - Helper Methods
+
+- (void)dismissAutoLockAndDeletePasscode
+{
+    UIViewController *viewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (viewController.presentedViewController) {
+        viewController = viewController.presentedViewController;
+        if ([viewController isKindOfClass:[VENTouchLockSplashViewController class]]) {
+            VENTouchLockSplashViewController *splashViewController = (VENTouchLockSplashViewController *)viewController;
+            [splashViewController dismissWithUnlockSuccess:YES unlockType:VENTouchLockSplashViewControllerUnlockTypePasscode animated:NO];
+        }
+    }
+    [[VENTouchLock sharedInstance] deletePasscode];
 }
 
 @end
