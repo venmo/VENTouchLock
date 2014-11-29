059fba221fdf6eeef27dd7b72965a10e35139028
dasmer <dasmersingh@gmail.com>
2014-11-29 14:04:44 -0500

    Separate tests each digit combination

--- VENTouchLockSample/VENTouchLockSampleTests/VENTouchLockSnapshotTests.m
+++ VENTouchLockSample/VENTouchLockSampleTests/VENTouchLockSnapshotTests.m
@@ -14,18 +14,44 @@
     self.recordMode = NO;
 }
 
-- (void)testPasscodeViewSnapshot
+- (void)testPasscodeViewSnapshotWithZeroDigits
 {
-    VENTouchLockPasscodeView *passcodeView;
-    for (NSUInteger i = 0; i <= 4; i++) {
-        passcodeView = [[VENTouchLockPasscodeView alloc] initWithTitle:@"Test Title" frame:CGRectMake(0, 0, 300, 300)];
-        for (NSUInteger j = 0; j < i; j++) {
-            VENTouchLockPasscodeCharacterView *characterView = (VENTouchLockPasscodeCharacterView *)passcodeView.characters[j];
-            characterView.isEmpty = NO;
-        }
-        NSString *identifier = [NSString stringWithFormat:@"%@Digits", [@(i) stringValue]];
-        FBSnapshotVerifyView(passcodeView, identifier);
+    VENTouchLockPasscodeView *passcodeView = [self passcodeViewWithNumberOfDigits:0];
+    FBSnapshotVerifyView(passcodeView, nil);
+}
+
+- (void)testPasscodeViewSnapshotWithOneDigit
+{
+    VENTouchLockPasscodeView *passcodeView = [self passcodeViewWithNumberOfDigits:1];
+    FBSnapshotVerifyView(passcodeView, nil);
+}
+
+- (void)testPasscodeViewSnapshotWithTwoDigits
+{
+    VENTouchLockPasscodeView *passcodeView = [self passcodeViewWithNumberOfDigits:2];
+    FBSnapshotVerifyView(passcodeView, nil);
+}
+
+- (void)testPasscodeViewSnapshotWithThreeDigits
+{
+    VENTouchLockPasscodeView *passcodeView = [self passcodeViewWithNumberOfDigits:3];
+    FBSnapshotVerifyView(passcodeView, nil);
+}
+
+- (void)testPasscodeViewSnapshotWithFourDigits
+{
+    VENTouchLockPasscodeView *passcodeView = [self passcodeViewWithNumberOfDigits:4];
+    FBSnapshotVerifyView(passcodeView, nil);
+}
+
+- (VENTouchLockPasscodeView *)passcodeViewWithNumberOfDigits:(NSUInteger)numberOfDigits
+{
+    VENTouchLockPasscodeView *passcodeView = [[VENTouchLockPasscodeView alloc] initWithTitle:@"Test Title" frame:CGRectMake(0, 0, 300, 300)];
+    for (NSUInteger j = 0; j < numberOfDigits; j++) {
+        VENTouchLockPasscodeCharacterView *characterView = (VENTouchLockPasscodeCharacterView *)passcodeView.characters[j];
+        characterView.isEmpty = NO;
     }
+    return passcodeView;
 }
 
 @end
