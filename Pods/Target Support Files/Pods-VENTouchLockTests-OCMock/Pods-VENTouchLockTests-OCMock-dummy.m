a97e6ebe5d4a5c1d65c29bc24df8831be4d98a84
Mark Adams <mark.adams@venmo.com>
2015-05-16 11:53:29 -0700

    Ok let's try something else...

--- Pods/Target Support Files/Pods-VENTouchLockTests-OCMock/Pods-VENTouchLockTests-OCMock-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VENTouchLockTests_OCMock : NSObject
-@end
-@implementation PodsDummy_Pods_VENTouchLockTests_OCMock
-@end
--- Pods/Target Support Files/Pods-VENTouchLockTests-Specta/Pods-VENTouchLockTests-Specta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VENTouchLockTests_Specta : NSObject
-@end
-@implementation PodsDummy_Pods_VENTouchLockTests_Specta
-@end
